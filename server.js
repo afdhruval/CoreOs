d4a64cb85d47a0a743cfce2dd6cd4279b1886c4a
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-18 00:01:14 +0530

    feat: add AI service with test function and integrate it into server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
+import { testAi } from "./src/services/ai.service.js";
 dotenv.config({ path: "./.env" });
 
 import app from "./src/app.js";
 import connectTOdb from "./src/config/database.js";
 
+testAi()
 
 connectTOdb()
 app.listen(3000, () => {
